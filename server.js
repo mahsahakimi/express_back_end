3b0f712151cb30c8f6722a50e88a14046bfe4205
mahsahakimi <mahsah4kimi@gmail.com>
2024-12-12 19:22:28 +0330

    problems endpoint added

--- server.js
+++ server.js
@@ -8,15 +8,56 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use('/login', (req, res) => {
-    res.send({
-      token: 'test123'
-    });
-  });
+let problems = [
+  {
+    "title" : "yoyo",
+    "content" : "Who is the most famous rapper in the hip-hop history?",
+    "options" : {
+        "0" : "Kendrick Lamar",
+        "1" : "Nas",
+        "2" : "Eminem",
+        "3" : "Reza Pishro"
+    },
+    "answer" : "2",
+    "difficulty" : "Easy",
+    "category" : "music",
+    "author" : "2"
+  },
+  {
+    "title" : "boom",
+    "content" : "Where was 2024 olympics held?",
+    "options" : {
+        "0" : "Iran",
+        "1" : "Paris",
+        "2" : "America",
+        "3" : "Japan"
+    },
+    "answer" : "1",
+    "difficulty" : "Medium",
+    "category" : "sport",
+    "author" : "5"
+  }
+]
 
-  app.get('/add/:a/:b', (req, res) => {
-    // res.send('<h1>Hello!</h1>');
-    res.json({'ans' : 5});
-  })
+// // READ - Get all problems
+app.get('/problems', (req, res) => {
+  res.json(problems);
+});
 
-  app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+// // app.use('/login', (req, res) => {
+// //     res.send({
+// //       token: 'test123'
+// //     });
+// //   });
+
+// //   app.get('/add/:a/:b', (req, res) => {
+// //     // res.send('<h1>Hello!</h1>');
+// //     res.json({'ans' : 5});
+// //   })
+
+const PORT = 8081;
+// app.listen(PORT, () => console.log('API is running on http://localhost:' + PROT + '/login'));
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
